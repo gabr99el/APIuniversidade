99fda4c5576ed5ae415ea2a2834079d2086025b9
gabr99el <wesllengabr11el2003x@gmail.com>
2024-01-02 19:46:45 -0300

    end

--- Controllers/AutorizaController.cs
+++ Controllers/AutorizaController.cs
@@ -8,87 +8,96 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authorization;
+//rever a identação 
 
 [ApiController]
-[Route("[controller]")]
+[Route ("[controller]")]
 public class AutorizaController : Controller
+
 {
     private readonly UserManager<IdentityUser> _userManager;
-    private readonly SignInManager<IdentityUser> _signInManager;
+
+    private readonly SignInManager<IdentityUser> _signInManager; 
 
     private readonly IConfiguration _configuration;
-
-    public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+    
+    public AutorizaController(UserManager<IdentityUser> userManager, 
+    SignInManager<IdentityUser> signInManager,IConfiguration configuration )
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _configuration = configuration;
+
     }
 
     [HttpGet]
-    public ActionResult<string> Get(){
+    public ActionResult<string> Get (){
         return "AutorizaController :: Acessado em : "
             + DateTime.Now.ToLongDateString();
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult> RegisterUser([FromBody]UsuarioDTO model){
+    public async Task<ActionResult> RegisterUser ([FromBody] UsuarioDTO model) {
         var user = new IdentityUser{
-        UserName = model.Email,
-        Email = model.Email,
-        EmailConfirmed = true
+            UserName= model.Email,
+            Email = model.Email,
+            EmailConfirmed = true
         };
 
-        var result = await _userManager.CreateAsync(user, model.Password);
-        if(!result.Succeeded)
-            return BadRequest(result.Errors);
-            
-        await _signInManager.SignInAsync(user, false);
-        return Ok(GeraToken(model));
-        }
-        
-        [HttpPost("login")]
-        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo){
-            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, 
-                isPersistent: false, lockoutOnFailure: false);
+        var result = await _userManager.CreateAsync (user, model.Password);
+        if (!result.Succeeded)
+            return BadRequest (result.Errors);
 
-            if(result.Succeeded)
-                return Ok(GeraToken(userInfo));
-            else{
-                ModelState.AddModelError(string.Empty,"Login inválido...");
-                return BadRequest(ModelState);
+        await _signInManager.SignInAsync (user, false);
+        return Ok (Geratoken(model));
+    }
+
+    [HttpPost("login")]
+        public async Task<ActionResult> Login ([FromBody] UsuarioDTO userInfo){
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, 
+                    isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+                return Ok(Geratoken(userInfo));
+            else {
+                ModelState.AddModelError(string.Empty, "Login Inválido...");
+                return BadRequest (ModelState);
             }
         }
 
-        private UsuarioToken GeraToken(UsuarioDTO userInfo){
-            var claims = new[]{
-                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
-                new Claim("IFRN","TecInfo"),
-                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+    private UsuarioToken Geratoken (UsuarioDTO userInfo){
+
+        var claims = new []{
+            new Claim (System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.UniqueName,userInfo.Email),
+            new Claim ("IFRN", "TecInfo"),
+            new Claim (Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            //gerar chave através de um algoritimo de chave simétrica
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
 
-            var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            //gerar a assinatura digital do token utilizado 
+            //a cave privada (key) e o algoritimo HMAC SHA 256
+             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            
+            //tempo de expiracao do token
+             var expiracao = _configuration ["TokenConfiguration:ExpireHours"];
+             var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
 
-            var expiracao = _configuration["TokenConfiguration:ExpireHours"];
-            var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
-
-            JwtSecurityToken token = new JwtSecurityToken(
+             JwtSecurityToken token = new JwtSecurityToken(
                 issuer: _configuration["TokenConfiguration:Issuer"],
-                audience: _configuration["TokenConfiguration:Audience"],
+                audience: _configuration ["TokenConfiguration:Audience"],
                 claims: claims,
                 expires: expiration,
                 signingCredentials: credentials
-            );
+             );
 
             return new UsuarioToken(){
                 Authenticated = true,
                 Expiration = expiration,
                 Token = new JwtSecurityTokenHandler().WriteToken(token),
-                Message = "JWT Ok."
+                Message = "JWT ok"
             };
-
-            
         }
-    }
+}
+
